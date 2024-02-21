572ac59609f5cfa9bb37e6c4eb3d86f734040d41
Dameon Mendoza <dameonmendoza@gmail.com>
2024-02-21 10:33:03 -0700

    updated server URL

--- src/helpers/api-communicator.ts
+++ src/helpers/api-communicator.ts
@@ -8,6 +8,14 @@ export const loginUser = async (email:string, password:string) =>{
     const data = await res.data;
     return data;
 }
+// export const logoutUser = async (email:string, password:string) =>{
+//     const res = await axios.post("user/login", {email, password});
+//     if(res.status !== 200) {
+//         throw new Error("Unable to Login");
+//     }
+//     const data = await res.data;
+//     return data;
+// }
 
 
 export const checkAuthStatus = async () =>{
--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,8 +1,52 @@
 //import React from 'react';
+//import { useContext } from "react";
+import { Avatar, Box } from "@mui/material";
+import { useAuth} from "../context/AuthContext";
 
 const Chat = () => {
-    return <div>Chat</div>
-
+   //const useAuth = () => useContext(AuthContext)
+  const auth = useAuth();
+    console.log(auth);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box sx={{ display: { md: "flex", xs: "none" } }}>
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              my: 2,
+              color: "black",
+              fontWeight: 700,
+              bgcolor: "white",
+              mx: "auto",
+            }}
+          >
+            {
+            //auth?.user?.name[0]
+          }
+          </Avatar>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Chat;
